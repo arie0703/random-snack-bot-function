26b910d1bebce6bcdedd16beaabb21a7cd6e1c7a
K.A <ari0703sophia@gmail.com>
2023-05-31 07:56:03 +0900

    不要なファイルを削除し、Lambda実行・ビルド用のファイルを作成

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,18 +1,3 @@
-terraform {
-    required_providers {
-        aws = {
-            source = "hashicorp/aws"
-            version = ">= 3.74.2"
-        }
-        template = {
-            source = "hashicorp/template"
-            version = ">= 2.2.0"
-        }
-    }
-    required_version = ">= 1.0"
-}
-
 provider "aws" {
-    profile = var.profile
-    region  = "ap-northeast-1"
+  region = "ap-northeast-1"
 }
